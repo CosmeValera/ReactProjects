718439a82eb25cd7b92ce026206b5e97ea2fa32c
CosmeValera <cosmevalerareales@gmail.com>
2022-12-09 11:36:12 +0100

    before trying challenge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+/**
+  Challenge: find out what happens if we try to append JSX
+  to our div#root using .append() instead of ReactDOM
+  1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
+  2. Select the div with the ID of "root" and use .append()` to append
+  your JSX
+  3. See if you can guess what will show up in the browser before running
+  the code
+  4. See if you can explain what actually shows up in the browser
+*/
+
 const App = () => {
   return (
     <div>
