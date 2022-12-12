b1c4ede7085620a190fb50d49d3fb13082b9a101
CosmeValera <cosmevalerareales@gmail.com>
2022-12-12 11:38:28 +0100

    separating Page parts into components

--- src/index.js
+++ src/index.js
@@ -1,37 +1,43 @@
-/**
-Challenge:
-
-Part 2:
-- Add a 'header` element with a nested `nav` element. Inside the `nav`,
-  include a `img` element with the image of the React logo inside
-  (src="./react-logo.png") and make sure to set the width to something
-  more manageable so it doesn't take up the whole screen
-- Add an `h1` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list.
-- Add a footer after the list that says:
-    "© 20xx <last name here> development. All rights reserved."
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Page() {
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src={require('./react-logo.png')} alt='logo-react' width='40px'/>
+      </nav>
+    </header>
+  );
+}
+
+function MainContent() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src={require('./react-logo.png')} alt='logo-react' width='40px'/>
-        </nav>
-      </header>
       <h1>Why I love working with react: </h1>
       <ol>
         <li>Because React seems entertaining</li>
         <li>Because it requires another mindset
           to work with</li>
       </ol>
-      <footer>
-        <small>© 2022 Valera Development. All rights reserved.</small>
-      </footer>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2022 Valera Development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
