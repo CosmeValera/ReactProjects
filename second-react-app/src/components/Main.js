28cf7d1edb151659fd23b467d04a81b098a8d4ec
CosmeValera <cosmevalerareales@gmail.com>
2022-12-14 14:05:52 +0100

    main content done

--- second-react-app/src/components/Main.js
+++ second-react-app/src/components/Main.js
@@ -1,8 +1,25 @@
+// Challenge: Build the main section!
+
+// Skip 2 aspects of the design for now:
+// 1. The colored bullets in the list
+// 2. The larger React logo on the side
+
+// Those will be separate challenges coming up
+
 import React from 'react';
 
 function Main() {
     return(
-        <h1>Main Component</h1>
+        <main className='main'>
+            <h1 className='main__header'>Fun facts about React</h1>
+            <ul className='main__list'>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K starts on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     );
 }
 
--- second-react-app/src/components/Navbar.js
+++ second-react-app/src/components/Navbar.js
@@ -4,7 +4,7 @@ import reactImg from "images/react-icon-small.png";
 function Navbar() {
     return (
         <div className="navbar">
-            <div className="navbar-left">
+            <div className="navbar__left">
                 <img src={reactImg} alt="logo-react" width="35px"/>
                 <h2>ReactFacts</h2>
             </div>
