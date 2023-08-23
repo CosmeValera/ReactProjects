3e1b1a9aaa759f255bac17bb60c1675245ef86be
CosmeValera <cosmevalerareales@gmail.com>
2023-08-23 19:40:32 +0200

    React third project created

---
+++ third-react-backup-airbnb-experience/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+
+/*
+Challenge: Build the Hero component.
+Check the Figma file for the design specifics.
+*/
+function App() {
+  return (
+    <div>
+      < Navbar />
+      < Hero /> 
+    </div>
+  );
+}
+
+export default App;
