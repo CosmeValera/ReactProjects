28827f570ac18af9d9ba6ccaa2ef4277b513e138
CosmeValera <cosmevalerareales@gmail.com>
2023-09-05 14:28:21 +0200

    create example file: state.jsx

---
+++ meme-generator/otherApps/state.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function App() {
+    const [isImportant, func] = React.useState("Yes")
+    console.log(isImportant)
+    return (
+        <div className="state">
+            <h1 className="state--title">Is state important to know?</h1>
+            <div className="state--value">
+                <h1>{isImportant}</h1>
+            </div>
+        </div>
+    )
+}
