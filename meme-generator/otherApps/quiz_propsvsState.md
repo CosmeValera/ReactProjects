f97eaf7629c6d8e98ab10f24db9f755ffe0ad12a
CosmeValera <cosmevalerareales@gmail.com>
2023-09-05 14:13:36 +0200

    add getMemeImag

---
+++ meme-generator/otherApps/quiz_propsvsState.md
@@ -0,0 +1,13 @@
+1. How would you describe the concept of "state"?
+
+
+
+2. When would you want to use props instead of state?
+
+
+
+3. When would you want to use state instead of props?
+
+
+
+4. What does "immutable" mean? Are props immutable? Is state immutable?
--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import memesData from "../memesData.jsx"
 
 export function Meme() {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return (
         <main>
-            <form className="form" action="">
+            <div className="form" action="">
                 <input type="text" placeholder="top text" className="form--input"/>
                 <input type="text" placeholder="bottom text" className="form--input"/>
-                <input type="button" value="Get a new meme image" className="form--button" />
-            </form>
+                <button onClick={getMemeImage} value="Get a new meme image" className="form--button"></button>
+            </div>
         </main>
     )
 }
