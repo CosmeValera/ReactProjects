8d234cc73e1742bfa83436bbc7b40ef9242669a6
CosmeValera <cosmevalerareales@gmail.com>
2023-09-05 22:57:01 +0200

    img state is changed when button pressed

--- meme-generator/src/components/Header.jsx
+++ meme-generator/src/components/Header.jsx
@@ -8,7 +8,7 @@ export function Header() {
                     src="./images/troll-face.png" 
                     className="header--image"
                 />
-                <h2 className="header--title">Header component</h2>
+                <h2 className="header--title">Meme Generator</h2>
                 <h4 className="header--project">React Course - Project 3</h4>
             </header>
         </header>
--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -2,21 +2,35 @@ import React from 'react';
 import memesData from "../memesData.jsx"
 
 export function Meme() {
+    /**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+    const [memeImage, setMemeImage] = React.useState('');
+    
 
     function getMemeImage() {
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        console.log(url)
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url;
+        console.log(url);
+        setMemeImage(url);
     }
 
     return (
         <main>
             <div className="form" action="">
-                <input type="text" placeholder="top text" className="form--input"/>
-                <input type="text" placeholder="bottom text" className="form--input"/>
-                <button onClick={getMemeImage} value="Get a new meme image" className="form--button"></button>
+                <input className="form--input" type="text" placeholder="top text"/>
+                <input className="form--input" type="text" placeholder="bottom text"/>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
             </div>
+            <img className='img' src={memeImage} alt=""/>
         </main>
     )
 }
