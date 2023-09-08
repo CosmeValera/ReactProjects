fb12de38bcfc404d060fbe8bead30c86705b61b5
CosmeValera <cosmevalerareales@gmail.com>
2023-09-08 18:41:57 +0200

    form, add values and textarea

--- meme-generator/otherApps/forms.jsx
+++ meme-generator/otherApps/forms.jsx
@@ -2,10 +2,10 @@ import React from "react"
 
 export default function Form() {
     const [formData, setFormData] = React.useState(
-        {firstName: "", lastName: ""}
+        {firstName: "", lastName: "", email: "", comments: ""}
     )
     
-    console.log(formData)
+    console.log(formData.comments)
     
     function handleChange(event) {
         setFormData(prevFormData => {
@@ -16,6 +16,11 @@ export default function Form() {
         })
     }
     
+    /**
+     * Challenge: Add a textarea for "comments" to the form
+     * Make sure to update state when it changes.
+     */
+    
     return (
         <form>
             <input
@@ -23,12 +28,27 @@ export default function Form() {
                 placeholder="First Name"
                 onChange={handleChange}
                 name="firstName"
+                value={formData.firstName}
             />
             <input
                 type="text"
                 placeholder="Last Name"
                 onChange={handleChange}
                 name="lastName"
+                value={formData.lastName}
+            />
+            <input
+                type="email"
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
+            />
+            <textarea 
+                value={formData.comments}
+                placeholder="Comments"
+                onChange={handleChange}
+                name="comments"
             />
         </form>
     )
