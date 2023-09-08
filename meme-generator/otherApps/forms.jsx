2a3ab0b7915ddd5f5a34af49b942257c1f46e8ab
CosmeValera <cosmevalerareales@gmail.com>
2023-09-08 18:50:54 +0200

    form add radio buttton

--- meme-generator/otherApps/forms.jsx
+++ meme-generator/otherApps/forms.jsx
@@ -2,24 +2,26 @@ import React from "react"
 
 export default function Form() {
     const [formData, setFormData] = React.useState(
-        {firstName: "", lastName: "", email: "", comments: ""}
+        {
+            firstName: "", 
+            lastName: "", 
+            email: "", 
+            comments: "", 
+            isFriendly: true,
+            employment: ""
+        }
     )
-    
-    console.log(formData.comments)
+    console.log(formData.employment)
     
     function handleChange(event) {
+        const {name, value, type, checked} = event.target
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
-                [event.target.name]: event.target.value
+                [name]: type === "checkbox" ? checked : value
             }
         })
     }
-    
-    /**
-     * Challenge: Add a textarea for "comments" to the form
-     * Make sure to update state when it changes.
-     */
     
     return (
         <form>
@@ -50,6 +52,54 @@ export default function Form() {
                 onChange={handleChange}
                 name="comments"
             />
+            <input 
+                type="checkbox" 
+                id="isFriendly" 
+                checked={formData.isFriendly}
+                onChange={handleChange}
+                name="isFriendly"
+            />
+            <label htmlFor="isFriendly">Are you friendly?</label>
+            <br />
+            <br />
+            
+            <fieldset>
+                <legend>Current employment status</legend>
+                
+                <input 
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    checked={formData.employment === "unemployed"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+                
+            </fieldset>
         </form>
     )
 }
