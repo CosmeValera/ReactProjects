4a26fd7918f229062a48ec1eab3705b02d282b27
CosmeValera <cosmevalerareales@gmail.com>
2023-09-08 18:14:23 +0200

    forms

---
+++ meme-generator/otherApps/forms.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+export default function Form() {
+    const [firstName, setFirstName] = React.useState("")
+    const [lastName, setLastName] = React.useState("")
+    /**
+     * Challenge: Track the applicant's last name as well
+     */
+    
+    console.log(firstName, lastName)
+    
+    function handleFirstNameChange(event) {
+        setFirstName(event.target.value)
+    }
+    
+    function handleLastNameChange(event) {
+        setLastName(event.target.value)
+    }
+    
+    return (
+        <form>
+            <input
+                type="text"
+                placeholder="First Name"
+                onChange={handleFirstNameChange}
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                onChange={handleLastNameChange}
+            />
+        </form>
+    )
+}
