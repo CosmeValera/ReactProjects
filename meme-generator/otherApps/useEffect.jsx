6ebe78042326333052de96b5c02f80ade3e9d095
CosmeValera <cosmevalerareales@gmail.com>
2023-09-11 02:01:58 +0200

    useeffect 2

--- meme-generator/otherApps/useEffect.jsx
+++ meme-generator/otherApps/useEffect.jsx
@@ -19,3 +19,33 @@ export default function App() {
         </div>
     )
 }
+
+
+// useEffect 2
+import React from "react"
+
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
+    
+    console.log("Component rendered")
+    
+    /**
+     * Challenge: re-write the useEffect
+     * It should run any time `count` changes
+     * For now, just console.log("Effect function ran")
+     */
+    React.useEffect(() => {
+        console.log("Effect function ran")
+    }, [count]);
+    
+    return (
+        <div>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+        </div>
+    )
+}
+
+
