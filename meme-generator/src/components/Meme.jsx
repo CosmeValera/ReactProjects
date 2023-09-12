740cd738608eb4966db0ee10d46714f052c485c8
CosmeValera <cosmevalerareales@gmail.com>
2023-09-12 13:22:22 +0200

    useEffect 3d option with await and async

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -52,6 +52,24 @@ export function Meme() {
     //         });
     // }, [])
     
+    /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below (3d option):
+    */
+    // React.useEffect(() => {
+    //     async function getMemes() {
+    //         const res = await fetch("https://api.imgflip.com/get_memes")
+    //         const data = await res.json()
+    //         setAllMemes(data.data.memes)
+    //     }
+    //     getMemes()
+    // }, [])
+    
 
     function handleChange(event) {
         const {name, value} = event.target;
