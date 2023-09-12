18d1bdc5709a393905ed0bd43613b77c08ad7eda
CosmeValera <cosmevalerareales@gmail.com>
2023-09-12 19:21:15 +0200

    toggle dark mode working

--- react-facts-with-theme/src/App.jsx
+++ react-facts-with-theme/src/App.jsx
@@ -3,10 +3,17 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 export default function App() {
+    const [isDarkMode, setIsDarkMode] = React.useState(false);
+
+    function handleToggleDarkMode() {
+        console.log("toggleDarkMode");
+        setIsDarkMode(prevIsDarkMode => !prevIsDarkMode);
+    }
+
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar darkMode={isDarkMode}  toggleDarkMode={handleToggleDarkMode}/>
+            <Main darkMode={isDarkMode}/>
         </div>
     )
 }
