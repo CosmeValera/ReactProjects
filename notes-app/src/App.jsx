9bee25370fa7d0cf4de5fd8825965915535cf838
CosmeValera <cosmevalerareales@gmail.com>
2023-09-15 00:59:16 +0200

    lazy state initialization of json.parse(localStorage.getitem('notes')) so it does only render the first time. To do it just add () =>, like it is a function

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -8,7 +8,7 @@ import {nanoid} from "nanoid"
 export default function App() {
     
     const [notes, setNotes] = React.useState(
-        JSON.parse(localStorage.getItem("notes")) || []
+        () => JSON.parse(localStorage.getItem("notes")) || []
     )
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
