c4c6fcb1d5f503d3bd40da3f5e4d7e406ead27ab
CosmeValera <cosmevalerareales@gmail.com>
2023-09-15 00:52:51 +0200

    add localstorage so it stores the text of the notes

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -5,19 +5,18 @@ import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 
-/**
- * Challenge: Spend 10-20+ minutes reading through the code
- * and trying to understand how it's currently working. Spend
- * as much time as you need to feel confident that you 
- * understand the existing code (although you don't need
- * to fully understand everything to move on)
- */
-
 export default function App() {
-    const [notes, setNotes] = React.useState([])
+    
+    const [notes, setNotes] = React.useState(
+        JSON.parse(localStorage.getItem("notes")) || []
+    )
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
+
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
     
     function createNewNote() {
         const newNote = {
