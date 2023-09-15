aaade28bb555ce8263d93a070fda7979968243ea
CosmeValera <cosmevalerareales@gmail.com>
2023-09-15 18:17:54 +0200

    give titles to the notes

--- notes-app/src/components/Sidebar.jsx
+++ notes-app/src/components/Sidebar.jsx
@@ -1,6 +1,12 @@
 import React from "react"
 
 export default function Sidebar(props) {
+    
+    function getNoteName(note) {
+        const title = JSON.stringify(note.body).split('\\n')[0].substring(1);
+        return title;
+    }
+    
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
@@ -10,7 +16,7 @@ export default function Sidebar(props) {
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{getNoteName(note)}</h4>
             </div>
         </div>
     ))
