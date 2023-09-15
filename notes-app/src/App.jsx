f7e6c09b990059b39d88ec41a27ebda22cc083ae
CosmeValera <cosmevalerareales@gmail.com>
2023-09-15 19:47:04 +0200

    small refactors

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 
@@ -11,8 +10,11 @@ export default function App() {
         () => JSON.parse(localStorage.getItem("notes")) || []
     )
     const [currentNoteId, setCurrentNoteId] = React.useState(
-        (notes[0] && notes[0].id) || ""
+        (notes[0]?.id) || ""
     )
+    const currentNote = 
+        notes.find(note => note.id === currentNoteId) 
+        || notes[0]
 
     React.useEffect(() => {
         localStorage.setItem("notes", JSON.stringify(notes));
@@ -70,13 +72,6 @@ export default function App() {
         setNotes(prevNotes => prevNotes.filter((note) => note.id !== noteId))
     }
     
-
-    function findCurrentNote() {
-        return notes.find(note => {
-            return note.id === currentNoteId
-        }) || notes[0]
-    }
-    
     return (
         <main>
         {
@@ -89,7 +84,7 @@ export default function App() {
             >
                 <Sidebar
                     notes={notes}
-                    currentNote={findCurrentNote()}
+                    currentNote={currentNote}
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
                     deleteNote={deleteNote}
@@ -98,7 +93,7 @@ export default function App() {
                     currentNoteId && 
                     notes.length > 0 &&
                     <Editor 
-                        currentNote={findCurrentNote()} 
+                        currentNote={currentNote} 
                         updateNote={updateNote} 
                     />
                 }
