fb172910bb1038640cd4f1e8938d407de78a4156
CosmeValera <cosmevalerareales@gmail.com>
2023-09-20 23:13:17 +0200

    we start the project from zero, remove everything

--- tenzies/src/index.jsx
+++ tenzies/src/index.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+/**
+ * Challenge: Start a brand new React app!
+ * - Create a separate App component
+ * - Import and render the App component here
+ * - In the App component, render a <main> element
+ * - Style everything to look like the slide
+ */
