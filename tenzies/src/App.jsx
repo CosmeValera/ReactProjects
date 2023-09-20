cc42e92264968fcc3eb2e228b149314d11943f87
CosmeValera <cosmevalerareales@gmail.com>
2023-09-20 23:52:27 +0200

    random values for dice

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -3,19 +3,22 @@ import React from "react";
 import Die from "./components/Die";
 
 export default function App() {
+
+  function allNewDice() {
+    const numbers = [];
+
+    for (let i = 0; i < 10; i ++) {
+      const randomNumber = Math.floor(Math.random() * 6) + 1;
+      numbers.push(<Die value={randomNumber} key={i}/>)
+    }
+    
+    return numbers;
+  }
+  
   return (
     <main>
       <div className="dice-container">
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
+        {allNewDice()}
       </div>
     </main>
   );
