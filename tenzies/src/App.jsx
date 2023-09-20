9c736192de48b74c72e27d096f921678332ece9b
CosmeValera <cosmevalerareales@gmail.com>
2023-09-21 00:07:22 +0200

    add button reroll dice

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -3,6 +3,14 @@ import React from "react";
 import Die from "./components/Die";
 
 export default function App() {
+  /**
+   * Challenge: Create a `Roll Dice` button that will re-roll
+   * all 10 dice
+   * 
+   * Clicking the button should generate a new array of numbers
+   * and set the `dice` state to that new array (thus re-rendering
+   * the array to the page)
+   */
 
   const [dice, setDice] = React.useState(allNewDice())
 
@@ -20,12 +28,17 @@ export default function App() {
   const diceElements = dice.map((die, index) => {
     return <Die value={die} key={index}/>
   })
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
   
   return (
     <main>
       <div className="dice-container">
         {diceElements}
       </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
   );
 }
