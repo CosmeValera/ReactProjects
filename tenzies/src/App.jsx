5d15d786139eaabc0a59a0371c2ceb3ad66e7458
CosmeValera <cosmevalerareales@gmail.com>
2023-09-20 23:24:36 +0200

    create index.jsx

---
+++ tenzies/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function App() {
+  return (
+    <main>Hola</main>
+  );
+}
--- tenzies/src/index.jsx
+++ tenzies/src/index.jsx
@@ -1,7 +1,10 @@
-/**
- * Challenge: Start a brand new React app!
- * - Create a separate App component
- * - Import and render the App component here
- * - In the App component, render a <main> element
- * - Style everything to look like the slide
- */
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root"));
