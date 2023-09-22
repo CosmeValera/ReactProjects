0cd8bc8e13edefeda52fec73e84bfee7324e5276
CosmeValera <cosmevalerareales@gmail.com>
2023-09-22 20:53:50 +0200

    add blue and yellow dots, so so positioned

--- quizzical/src/components/Menu.jsx
+++ quizzical/src/components/Menu.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 export default function Menu(props) {
     return (
-        <>
-            <h1>Menu</h1>
+        <div className="menu-container">
+            <h1>Quizzical</h1>
+            <p>Will you be able to answer all the questions? 😳😈</p>
             <button onClick={props.toggleQuizState}>Start Quiz</button>
-        </>
+            <img src="./../../public/img/blue-blob.svg" className="blue-dot" alt="blue dot"/>
+            <img src="./../../public/img/yellow-blob.svg" className="yellow-dot" alt="yellow dot"/>
+        </div>
     )
 }
