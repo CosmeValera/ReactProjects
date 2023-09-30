d2f8ed7521a228cc7ed087c6e5517b5601ed0101
CosmeValera <cosmevalerareales@gmail.com>
2023-09-30 20:55:03 +0200

    make data in quiz contain an array of questions, not just 1

--- quizzical/src/components/Quiz.jsx
+++ quizzical/src/components/Quiz.jsx
@@ -2,37 +2,34 @@ import React from "react";
 import he from "he";
 
 export default function Quiz(props) {
-    const [data, setData] = React.useState({
-        "category": "Celebrities",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "What is the real name of the Scout in &quot;Team Fortress 2&quot;?",
-        "correct_answer": "False",
-        "incorrect_answers": [
-          "True"
-        ]
-    })
+    const [data, setData] = React.useState([{
+        "category": "",
+        "type": "",
+        "difficulty": "",
+        "question": "",
+        "correct_answer": "",
+        "incorrect_answers": ["", "", ""]
+    }])
 
     React.useEffect(() => {
-        console.log("1");
         fetch('https://opentdb.com/api.php?amount=1')
         .then(result=> result.json())
         .then(resultJson => {
-            setData(resultJson.results[0])
+            setData(resultJson.results)
         })
-        console.log("2");
     }, []);
-    
-    React.useEffect(()=> console.log(data), [data])
+
+    // let questionContainer = A`
+    //     <h3>{he.decode(data[0].question)}</h3>
+    //     <p>{he.decode(data[0].correct_answer)}</p>
+    //     <p>{he.decode(data[0].incorrect_answers[0])}</p>
+    // `
 
     return (
         <>
-            <h1>Quiz</h1>
-            <h2>Question: </h2>
-            <p>{he.decode(data.question)}</p>
-            <h2>Answers: </h2>
-            <p>{he.decode(data.correct_answer)}</p>
-            <p>{he.decode(data.incorrect_answers[0])}</p>
+            <h3>{he.decode(data[0].question)}</h3>
+            <p>{he.decode(data[0].correct_answer)}</p>
+            <p>{he.decode(data[0].incorrect_answers[0])}</p>
 
             <button className="btn-primary" onClick={props.toggleQuizState}>Back</button>
         </>
