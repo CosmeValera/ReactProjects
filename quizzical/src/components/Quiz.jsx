7f617f5f71aebfa6364d50ee0e786856a2ef547c
CosmeValera <cosmevalerareales@gmail.com>
2023-10-09 20:48:07 +0200

    options appear now at random positions and not always correct option first

--- quizzical/src/components/Quiz.jsx
+++ quizzical/src/components/Quiz.jsx
@@ -21,32 +21,40 @@ export default function Quiz(props) {
         })
     }, []);
 
+    function shuffleArray(array) {
+        const shuffledArray = [...array]
+        for (let i = shuffledArray.length -1 ; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
+        }
+        return shuffledArray;
+    }
+
     function buttonSelected(evt) {
         const button = evt.currentTarget;
         const questionContainer = button.parentNode;
-        questionContainer.querySelectorAll("button").forEach((button) => {
-            button.classList.remove("btn-selected")
+        questionContainer.querySelectorAll("button").forEach((but) => {
+            but.classList.remove("btn-selected")
         });
         button.classList.add("btn-selected");
     }
 
-    /**
-     * Next things to do:
-     * 1. change p for correct and incorrect answers to button
-     * 2. add styles to the buttons (same ::active effect)
-     * 3. randomize order, shouldn't always be the correct answer first
-     */
-
     const questionsToAnswer = data.map((questionInstance, index) => {
         const { question, correct_answer, incorrect_answers } = questionInstance;
         
+        // Shuffle the incorrect answers and insert the correct answer at a random position
+        const shuffledOptions = shuffleArray(incorrect_answers)
+        const randomIndex = Math.floor(Math.random() * (shuffledOptions.length + 1))
+        shuffledOptions.splice(randomIndex, 0, correct_answer)
+
         return <>
             <div className="question-container" key={index}>
                 <h3>{he.decode(question)}</h3>
-                <button className="btn-option btn-selected" onClick={buttonSelected}>{he.decode(correct_answer)}</button>
-                {incorrect_answers.map( (incorrectAnswer, i) => (
-                    <button className="btn-option" onClick={buttonSelected} key={`${index}-${i}`}>{ he.decode(incorrectAnswer) }</button>
-                ))}
+                {
+                    shuffledOptions.map((answer, i) => {
+                        return <button className="btn-option" key={`${index}-${i}`} onClick={buttonSelected}>{he.decode(answer)}</button>
+                    })
+                }
             </div>
             <hr/>
         </>
