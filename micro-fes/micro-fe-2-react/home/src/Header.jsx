ca05fd453ad74ea3fda308fc5d0d3178ef4376e5
Cosme Valera <cosmevalerareales@gmail.com>
2023-10-13 12:31:21 +0200

    add error handling for pdp page, if contract in home changes

--- micro-fes/micro-fe-2-react/home/src/Header.jsx
+++ micro-fes/micro-fe-2-react/home/src/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default function Header() {
+export default function Header({ app }) {
     return <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
-        Fidget Spinner World
+        Fidget Spinner World | {app.name}
     </div>
 }
--- micro-fes/micro-fe-2-react/pdp/src/App.jsx
+++ micro-fes/micro-fe-2-react/pdp/src/App.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
+import SafeComponent from "./SafeComponent";
 import Header from "home/Header";
 import Footer from "home/Footer";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
-    <Header />
-     <div className="my-10">PDP Page Content </div>
-      <Footer />
+    <SafeComponent>
+      <Header/>
+    </SafeComponent>
+    <div className="my-10">PDP Page Content </div>
+    <Footer />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
