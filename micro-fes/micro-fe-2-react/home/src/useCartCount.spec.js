556e75f5f9ac395b8d1d58ba5fb72d042dde2091
Cosme Valera <cosmevalerareales@gmail.com>
2023-10-17 17:05:44 +0200

    add 2nd unit testing test. This second one checks when the cart has 1 item

--- micro-fes/micro-fe-2-react/home/src/useCartCount.spec.js
+++ micro-fes/micro-fe-2-react/home/src/useCartCount.spec.js
@@ -2,16 +2,28 @@ import { renderHook, act } from "@testing-library/react-hooks";
 
 import { useCartCount } from "./useCartCount";
 
-jest.mock("cart/cart", ()=> ({
+let callback = () => {};
+
+jest.mock("cart/cart", () => ({
     cart: {
         cartItems: [],
-        subscribe: jest.fn(),
-    }
+        subscribe: (cb) => {
+            callback = cb;
+        },
+    },
 }));
 
 describe("useCartCount", () => {
     it("should return cart count", () => {
         const { result } = renderHook(() => useCartCount());
         expect(result.current).toBe(0);
+    });
+
+    it("should return cart count", () => {
+        const { result } = renderHook(() => useCartCount());
+        act(() => {
+            callback({ cartItems: [{ id: 1 }] });
+        });
+        expect(result.current).toBe(1);
     });
 });
