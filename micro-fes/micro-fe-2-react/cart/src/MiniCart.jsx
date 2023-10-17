b1c489ef0b03d12cec2fdeca22c490610735f7d9
Cosme Valera <cosmevalerareales@gmail.com>
2023-10-17 12:52:21 +0200

    add in home page button: 'add to cart'

--- micro-fes/micro-fe-2-react/cart/src/MiniCart.jsx
+++ micro-fes/micro-fe-2-react/cart/src/MiniCart.jsx
@@ -31,7 +31,7 @@ export default function MiniCart() {
                         style={{
                             width: 300,
                             top: "2rem",
-                            // left: -250,
+                            left: -250,
                         }}
                     >
                         <div 
--- micro-fes/micro-fe-2-react/home/src/Header.jsx
+++ micro-fes/micro-fe-2-react/home/src/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
             <div className="flex">
                 <div className="flex-grow">Fidget Spinner World</div>
-                <div className="flex-end">
+                <div className="flex-end relative">
                     <MiniCart />
                     <Login />
                 </div>
