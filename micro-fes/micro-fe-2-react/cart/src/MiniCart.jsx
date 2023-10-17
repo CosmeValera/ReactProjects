b1f794e6cdc28adec03c5b0c1c07c38498b8ed36
Cosme Valera <cosmevalerareales@gmail.com>
2023-10-17 12:21:26 +0200

    cart page: when click cart icon it shows preview of your items to but

--- micro-fes/micro-fe-2-react/cart/src/MiniCart.jsx
+++ micro-fes/micro-fe-2-react/cart/src/MiniCart.jsx
@@ -24,6 +24,36 @@ export default function MiniCart() {
                 <i className="ri-shopping-cart-2-fill text-2xl" id="showcart"></i>
                 {items.length}
             </span>
+            {showCart && (
+                <>
+                    <div 
+                        className="absolute p-5 border-4 border-blue-800 bg-white rounded-xl text-black"
+                        style={{
+                            width: 300,
+                            top: "2rem",
+                            // left: -250,
+                        }}
+                    >
+                        <div 
+                            className="grid gap-3 text-sm"
+                            style={{
+                                gridTemplateColumns: "1fr 3fr 10fr 2fr",
+                            }}
+                        >
+                            {items.map((item)=> (
+                                <React.Fragment key={item.id}>
+                                    <div>{item.quantity}</div>
+                                    <img src={item.image} alt={item.name} className="max-h-6"/>
+                                    <div>{item.name}</div>
+                                    <div className="text-right">
+                                        {currency.format(item.quantity * item.price)}
+                                    </div>
+                                </React.Fragment>
+                            ))}
+                        </div>
+                    </div>
+                </>
+            )}
         </>
     )
 }
