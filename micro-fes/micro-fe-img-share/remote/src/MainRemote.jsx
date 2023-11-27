00d50bf93fd346ce0fae11af060da32465dd66a8
CosmeValera <cosmevalerareales@gmail.com>
2023-11-27 12:06:41 +0100

    Put img in src/img folder

--- micro-fes/micro-fe-img-share/remote/src/MainRemote.jsx
+++ micro-fes/micro-fe-img-share/remote/src/MainRemote.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./index.css";
-import reactImg from './React.svg.png';
+import reactImg from './img/React.svg.png';
 
 const MainRemote = () => (
   <div className="container">
     <div>Name: remote</div>
     <div>Framework: react</div>
-    <div>Language: JasdasvaScript</div>
+    <div>Language: JasdasvsaScript</div>
     <div>CSS: Empty CSS</div>
     <img src={reactImg} alt="No React img found" />
   </div>
