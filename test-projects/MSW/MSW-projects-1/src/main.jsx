4952a0dba1bddac249880028644129fe3314c6b8
CosmeValera <cosmevalerareales@gmail.com>
2024-02-13 11:25:49 +0100

    [MSW] Mocking enabled.

--- test-projects/MSW/MSW-projects-1/src/main.jsx
+++ test-projects/MSW/MSW-projects-1/src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+async function deferRender() {
+  const { worker } = await import('./mocks/browser.js');
+  return worker.start();
+}
+
+deferRender().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
+
