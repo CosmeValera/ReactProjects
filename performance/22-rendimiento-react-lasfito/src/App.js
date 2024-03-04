04c2388ec055dbd54c66890a1bd30be572a747c0
CosmeValera <cosmevalerareales@gmail.com>
2024-03-04 17:49:44 +0100

    Make 22-rendimiento-react-lasfito at 0 instead of 100. (Now it does not remove unnecesary renders, that's what I will be working on now)

--- performance/22-rendimiento-react-lasfito/src/App.js
+++ performance/22-rendimiento-react-lasfito/src/App.js
@@ -2,21 +2,12 @@ import { useState, memo } from "react";
 import { Layout, Button, VisualComponent } from "./componentes";
 
 const Alpha = () => {
-  const Contador = () => {
-    const [counter, setCounter] = useState(0);
-    return (
-      <>
-        <h4 className="-mt-2 mb-1  font-thin"> {counter}</h4>
-
-        <Button onClick={() => setCounter(counter + 1)}>
-          Actualizar Estado
-        </Button>
-      </>
-    );
-  };
+  const [counter, setCounter] = useState(0);
   return (
-    <VisualComponent title="Alpha" metodo="abstracción">
-      <Contador />
+    <VisualComponent title="Alpha">
+      <h4 className="-mt-2 mb-1 font-thin"> {counter}</h4>
+
+      <Button onClick={() => setCounter(counter + 1)}>Actualizar Estado</Button>
       <Bravo />
     </VisualComponent>
   );
@@ -26,49 +17,49 @@ const Bravo = () => {
   const [counter, setCounter] = useState(0);
   return (
     <VisualComponent title="Bravo">
-      <h4 className="-mt-2 mb-1  font-thin"> {counter}</h4>
+      <h4 className="-mt-2 mb-1 font-thin"> {counter}</h4>
 
       <Button onClick={() => setCounter(counter + 1)}>Actualizar Estado</Button>
       <div className="grid grid-rows-2 md:grid-cols-2 md:gap-4 md:grid-rows-1">
         <Charlie />
-        <Fetch hijo={<Fetch2 />} />
+        <Fetch />
       </div>
     </VisualComponent>
   );
 };
 
-const Charlie = memo(() => {
+const Charlie = () => {
   const [counter, setCounter] = useState(0);
-
   return (
-    <VisualComponent title="Charlie" metodo="memo">
-      <h4 className="-mt-2 mb-1  font-thin"> {counter}</h4>
+    <VisualComponent title="Charlie">
+      <h4 className="-mt-2 mb-1 font-thin"> {counter}</h4>
 
       <Button onClick={() => setCounter(counter + 1)}>Actualizar Estado</Button>
       <CharlieJr />
     </VisualComponent>
   );
-});
+};
+
 const CharlieJr = () => {
   const [counter, setCounter] = useState(0);
   return (
     <VisualComponent title="CharlieJr">
-      <h4 className="-mt-2 mb-1  font-thin"> {counter}</h4>
+      <h4 className="-mt-2 mb-1 font-thin"> {counter}</h4>
 
       <Button onClick={() => setCounter(counter + 1)}>Actualizar Estado</Button>
     </VisualComponent>
   );
 };
 
-const Fetch = (props) => {
+const Fetch = () => {
   const [counter, setCounter] = useState(0);
 
   return (
-    <VisualComponent title="Fetch" metodo="props">
-      <h4 className="-mt-2 mb-1  font-thin"> {counter}</h4>
+    <VisualComponent title="Fetch">
+      <h4 className="-mt-2 mb-1 font-thin"> {counter}</h4>
 
       <Button onClick={() => setCounter(counter + 1)}>Fetch Api</Button>
-      {props.hijo}
+      <Fetch2 />
     </VisualComponent>
   );
 };
@@ -77,7 +68,7 @@ const Fetch2 = () => {
 
   return (
     <VisualComponent title="Fetch 2">
-      <h4 className="-mt-2 mb-1  font-thin"> {counter}</h4>
+      <h4 className="-mt-2 mb-1 font-thin"> {counter}</h4>
 
       <Button onClick={() => setCounter(counter + 1)}>Fetch Api</Button>
     </VisualComponent>
