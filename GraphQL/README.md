baa3c4df451eb44f2548133ab1271529045655db
CosmeValera <cosmevalerareales@gmail.com>
2024-03-13 12:56:33 +0100

    Server up

--- GraphQL/README.md
+++ GraphQL/README.md
@@ -2,3 +2,12 @@
 A query language for your API.
 
 ![Grapqhl img](graphql.png)
+
+## 🚀 Getting Started
+We are installing GraphQL and Apollo Server:
+```bash
+npm i graphql apollo-server
+```
+
+### 📄 Docs
+`index.js`: the server.
--- GraphQL/index.js
+++ GraphQL/index.js
@@ -1,1 +1,54 @@
-;
+const { gql, ApolloServer } = require('apollo-server');
+
+const people = [
+    {
+        name: "Midu",
+        phone: "034-1234567",
+        street: "Calle Frontend",
+        city: "Barcelona",
+        id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+    },
+    {
+        name: "Youseff",
+        phone: "044-1234567",
+        street: "Avenida Fullstack",
+        city: "Mataro",
+        id: "3d599470-3436-11e9-bc57-8b80ba54c431"
+    },
+    {
+        name: "Itzi",
+        street: "Calle Frontend",
+        city: "Barcelona",
+        id: "3d599471-3436-11e9-bc57-8b80ba54c431"
+    },
+]
+
+const typeDefs = gql`
+    type Person {
+        name: String!
+        phone: String
+        street: String!
+        city: String!
+        id: ID!
+    }
+
+    type Query {
+        personCount: Int!
+        allPeople: [Person]!
+    }
+`
+
+const resolvers = {
+    Query: {
+        personCount: () => people.length,
+        allPeople: () => people
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs, resolvers
+})
+
+server.listen().then(({url}) => {
+    console.log(`Server ready at ${url}`)
+})
