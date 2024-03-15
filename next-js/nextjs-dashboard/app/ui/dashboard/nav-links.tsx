6292fb724c3cee95a1c9d6cf630cdb218254e967
CosmeValera <cosmevalerareales@gmail.com>
2024-03-15 14:15:40 +0100

    Add bg color for selected nav option. Need to add 'use client' at the top, because it's a Client component. And by default Next.js components are created first in server

--- next-js/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ next-js/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,12 @@
+'use client'
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -18,6 +21,8 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathName = usePathname();
+
   return (
     <>
       {links.map((link) => {
@@ -26,7 +31,9 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 
+            ${pathName === link.href ? 'bg-sky-100 text-blue-600' : ''}
+            `}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
