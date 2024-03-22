6563937ccfd72f679ae11d27ec49b2d59356533d
CosmeValera <cosmevalerareales@gmail.com>
2024-03-22 13:49:45 +0100

    Add ronin-database

--- performance/docker/node-docker/server.js
+++ performance/docker/node-docker/server.js
@@ -1,7 +1,22 @@
 const ronin = require('ronin-server')
+const database = require('ronin-database')
 const mocks = require('ronin-mocks')
 
-const server = ronin.server()
+async function main () {
+    try {
+        await database.connect(process.env.CONNECTIONSTRING)
 
-server.use('/' ,mocks.server(server.Router(), false, true))
-server.start()
+        const server = ronin.server({
+            port: process.env.SERVER_PORT
+        })
+        
+        server.use('/' , mocks.server(server.Router()))
+        const result = server.start()
+        console.log(result)
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+main()
