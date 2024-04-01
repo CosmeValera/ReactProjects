ed26bd8ff55b5b97514ef2aa614e5ba36e4dcd6b
CosmeValera <cosmevalerareales@gmail.com>
2024-04-01 16:55:21 +0200

    Add getAll()

--- performance/snmp/SNMP-2-React/server/app.js
+++ performance/snmp/SNMP-2-React/server/app.js
@@ -12,8 +12,8 @@ const oidMapping = {
   hrSystemProcesses: '1.3.6.1.2.1.25.1.6.0',
   hrSystemMaxProcesses: '1.3.6.1.2.1.25.1.7.0',
   hrMemorySize: '1.3.6.1.2.1.25.2.2.0',
-  hrStorageTable: '1.3.6.1.2.1.25.2.3.0',
-  hrStorageEntry: '1.3.6.1.2.1.25.2.3.1.0',
+  // hrStorageTable: '1.3.6.1.2.1.25.2.3.0',
+  // hrStorageEntry: '1.3.6.1.2.1.25.2.3.1.0',
   hrStorageIndex: '1.3.6.1.2.1.25.2.3.1.1.1',
   hrStorageType: '1.3.6.1.2.1.25.2.3.1.2.1',
   hrStorageDescr: '1.3.6.1.2.1.25.2.3.1.3.1',
@@ -29,19 +29,19 @@ const oidMapping = {
   hrDeviceID: '1.3.6.1.2.1.25.3.2.1.4.196608',
   hrDeviceStatus: '1.3.6.1.2.1.25.3.2.1.5.196608',
   hrDeviceErrors: '1.3.6.1.2.1.25.3.2.1.6.262145',
-  hrProcessorTable: '1.3.6.1.2.1.25.3.3.196608',
-  hrProcessorEntry: '1.3.6.1.2.1.25.3.3.1.196608',
+  // hrProcessorTable: '1.3.6.1.2.1.25.3.3.196608',
+  // hrProcessorEntry: '1.3.6.1.2.1.25.3.3.1.196608',
   hrProcessorFrwID: '1.3.6.1.2.1.25.3.3.1.1.196608',
   hrProcessorLoad: '1.3.6.1.2.1.25.3.3.1.2.196608',
-  hrNetworkTable: '1.3.6.1.2.1.25.3.4.262145',
-  hrNetworkEntry: '1.3.6.1.2.1.25.3.4.1.262145',
+  // hrNetworkTable: '1.3.6.1.2.1.25.3.4.262145',
+  // hrNetworkEntry: '1.3.6.1.2.1.25.3.4.1.262145',
   hrNetworkIfIndex: '1.3.6.1.2.1.25.3.4.1.1.262145',
   // hrPrinterTable: '1.3.6.1.2.1.25.3.5.0',
   // hrPrinterEntry: '1.3.6.1.2.1.25.3.5.1.0',
   // hrPrinterStatus: '1.3.6.1.2.1.25.3.5.1.1.0',
   // hrPrinterDetectedErrorState: '1.3.6.1.2.1.25.3.5.1.2.0',
-  hrDiskStorageTable: '1.3.6.1.2.1.25.3.6.393232',
-  hrDiskStorageEntry: '1.3.6.1.2.1.25.3.6.1.393232',
+  // hrDiskStorageTable: '1.3.6.1.2.1.25.3.6.393232',
+  // hrDiskStorageEntry: '1.3.6.1.2.1.25.3.6.1.393232',
   hrDiskStorageAccess: '1.3.6.1.2.1.25.3.6.1.1.393232',
   hrDiskStorageMedia: '1.3.6.1.2.1.25.3.6.1.2.393232',
   hrDiskStorageRemoveble: '1.3.6.1.2.1.25.3.6.1.3.393232',
@@ -53,8 +53,8 @@ const oidMapping = {
   // hrPartitionID: '1.3.6.1.2.1.25.3.7.1.3.0',
   // hrPartitionSize: '1.3.6.1.2.1.25.3.7.1.4.0',
   // hrPartitionFSIndex: '1.3.6.1.2.1.25.3.7.1.5.0',
-  hrFSTable: '1.3.6.1.2.1.25.3.8.1',
-  hrFSEntry: '1.3.6.1.2.1.25.3.8.1.1',
+  // hrFSTable: '1.3.6.1.2.1.25.3.8.1',
+  // hrFSEntry: '1.3.6.1.2.1.25.3.8.1.1',
   hrFSIndex: '1.3.6.1.2.1.25.3.8.1.1.1',
   hrFSMountPoint: '1.3.6.1.2.1.25.3.8.1.2.1',
   hrFSRemoteMountPoint: '1.3.6.1.2.1.25.3.8.1.3.1',
@@ -65,24 +65,23 @@ const oidMapping = {
   hrFSLastFullBackupDate: '1.3.6.1.2.1.25.3.8.1.8.1',
   hrFSLastPartialBackupDate: '1.3.6.1.2.1.25.3.8.1.9.1',
   // hrSWOSIndex: '1.3.6.1.2.1.25.3.9.0',
-  hrSWRunTable: '1.3.6.1.2.1.25.4.2.1',
-  hrSWRunEntry: '1.3.6.1.2.1.25.4.2.1.1',
+  // hrSWRunTable: '1.3.6.1.2.1.25.4.2.1',
+  // hrSWRunEntry: '1.3.6.1.2.1.25.4.2.1.1',
   hrSWRunIndex: '1.3.6.1.2.1.25.4.2.1.1.1',
-
   hrSWRunName: '1.3.6.1.2.1.25.4.2.1.2.1',
   hrSWRunID: '1.3.6.1.2.1.25.4.2.1.3.1',
   hrSWRunPath: '1.3.6.1.2.1.25.4.2.1.4.1',
   hrSWRunParameters: '1.3.6.1.2.1.25.4.2.1.5.1',
   hrSWRunType: '1.3.6.1.2.1.25.4.2.1.6.1',
   hrSWRunStatus: '1.3.6.1.2.1.25.4.2.1.7.1',
-  hrSWRunPerfTable: '1.3.6.1.2.1.25.5.1.1',
-  hrSWRunPerfEntry: '1.3.6.1.2.1.25.5.1.1.1',
+  // hrSWRunPerfTable: '1.3.6.1.2.1.25.5.1.1',
+  // hrSWRunPerfEntry: '1.3.6.1.2.1.25.5.1.1.1',
   hrSWRunPerfCPU: '1.3.6.1.2.1.25.5.1.1.1.1',
   hrSWRunPerfMem: '1.3.6.1.2.1.25.5.1.1.2.1',
-  // hrSWInstalledLastChange: '1.3.6.1.2.1.25.6.1.0',
+  hrSWInstalledLastChange: '1.3.6.1.2.1.25.6.1.0',
   hrSWInstalledLastUpdateTime: '1.3.6.1.2.1.25.6.2.0',
   // hrSWInstalledTable: '1.3.6.1.2.1.25.6.3.0',
-  hrSWInstalledEntry: '1.3.6.1.2.1.25.6.3.1.0',
+  // hrSWInstalledEntry: '1.3.6.1.2.1.25.6.3.1.0',
   hrSWInstalledIndex: '1.3.6.1.2.1.25.6.3.1.1.1',
   hrSWInstalledName: '1.3.6.1.2.1.25.6.3.1.2.1',
   hrSWInstalledID: '1.3.6.1.2.1.25.6.3.1.3.1',
@@ -90,17 +89,50 @@ const oidMapping = {
   hrSWInstalledDate: '1.3.6.1.2.1.25.6.3.1.5.1',
 };
 
-app.get('/', (req, res) => {
-  // Call snmpResponse function to retrieve SNMP data
-  snmpResponse('hrStorageAllocationUnits', (error, snmpData) => {
-    if (error) {
-      res.status(500).send('Error fetching SNMP data');
-    } else {
-      // Send the SNMP data as the response
-      console.log(snmpData)
-      res.send(snmpData);
-    }
-  });
+const getAll = (async (req, res) => {
+  const results = {};
+
+  const snmpPromises = [];
+
+  for (const objectName in oidMapping) {
+    console.log(objectName);
+    // Create a promise for each SNMP request and push it to the array
+    const promise = new Promise((resolve, reject) => {
+      snmpResponse(objectName, (error, snmpData) => {
+        if (error) {
+          console.error(`Error fetching SNMP data for ${objectName}:`, error);
+          results[objectName] = `Error fetching SNMP data: ${error.message}`;
+        } else {
+          results[objectName] = snmpData;
+        }
+        resolve(); // Resolve the promise regardless of error or success
+      });
+    });
+    snmpPromises.push(promise);
+  }
+
+  try {
+    await Promise.all(snmpPromises);
+    res.send(results);
+  } catch (error) {
+    console.error("An error occurred while fetching SNMP data:", error);
+    res.status(500).send('Error fetching SNMP data');
+  }
+});
+
+app.get('/', async (req, res) => {
+  await getAll(req, res);
+
+//   // Call snmpResponse function to retrieve SNMP data
+//   snmpResponse('hrStorageAllocationUnits', (error, snmpData) => {
+//     if (error) {
+//       // res.status(500).send('Error fetching SNMP data');
+//     } else {
+//       // Send the SNMP data as the response
+//       console.log(snmpData)
+//       res.send(snmpData);
+//     }
+//   });
 });
 
 app.listen(4001, () => {
@@ -117,7 +149,6 @@ const snmpResponse = (objectName, callback) => {
 
   // Define the OID to query
   const oid = oidMapping[objectName];
-
 
   // Perform an SNMP GET request for the specified OID
   session.get([oid], (error, varbinds) => {
