8c9e7e0e2bd4ad3cc30aa79c1fd2483ccf9874f1
CosmeValera <cosmevalerareales@gmail.com>
2024-05-31 13:05:50 +0200

    Lazy rendering in vite. No Advancement yet

--- micro-fes/Analysis of microfrontends/micro-fe-vite-and-webpack-interoperability/micro-fe-vite-is-host-webpack-is-mf/vite-host/src/App.jsx
+++ micro-fes/Analysis of microfrontends/micro-fe-vite-and-webpack-interoperability/micro-fe-vite-is-host-webpack-is-mf/vite-host/src/App.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import WebpackRemote from 'webpack_remote/WebpackApp'
+// import WebpackRemote from 'webpack_remote/WebpackApp'
+// console.log(WebpackRemote)
 
 function App() {
+  const [RemoteComponent, setRemoteComponent] = useState(null);
+
+  useEffect(() => {
+    const loadComponent = async () => {
+      const remoteModule = await import('webpack_remote/WebpackApp');
+      setRemoteComponent(() => remoteModule.default);
+    };
+    loadComponent();
+  }, []);
+
+  useEffect(() => {
+    console.log(RemoteComponent)
+  }, [RemoteComponent])
+
   return (
     <>
       <a href="https://vitejs.dev" target="_blank">
         <img src={viteLogo} className="logo" alt="Vite logo" />
       </a>
       <h1>VITE HOST</h1>
-      <WebpackRemote />
+      {RemoteComponent ? <RemoteComponent /> : <p>Loading remote component...</p>}
     </>
   )
 }
