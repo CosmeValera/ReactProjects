eada12deae401130565898d97a108fb93aba834f
CosmeValera <cosmevalerareales@gmail.com>
2024-05-31 15:11:42 +0200

    Remove unnecesarry webpack attributes

--- micro-fes/Analysis of microfrontends/micro-fe-vite-and-webpack-interoperability/micro-fe-vite-is-host-webpack-is-mf/vite-host/vite.config.js
+++ micro-fes/Analysis of microfrontends/micro-fe-vite-and-webpack-interoperability/micro-fe-vite-is-host-webpack-is-mf/vite-host/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import federation from '@originjs/vite-plugin-federation'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
--- micro-fes/Analysis of microfrontends/micro-fe-vite-and-webpack-interoperability/micro-fe-vite-is-host-webpack-is-mf/webpack-remote/webpack.config.js
+++ micro-fes/Analysis of microfrontends/micro-fe-vite-and-webpack-interoperability/micro-fe-vite-is-host-webpack-is-mf/webpack-remote/webpack.config.js
@@ -5,24 +5,12 @@ const path = require('path');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
 
-  mode: "development",
-  cache: false,
   target: 'web',
-  devtool: false,
-  entry: path.resolve(__dirname, "./src/index.js"),
-  
+
   output: {
     publicPath: "http://localhost:4002/",
-    libraryExport: 'index',
     chunkLoading: false,
-    // path: path.resolve(__dirname, 'dist'),
-    // module: true, // Disable for CJ
-    // library: {
-    //   type: "var",
-    //   name: "webpack_host"
-    // },
   },
-  // target: 'es2020', // Disable for CJ
 
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
@@ -62,9 +50,6 @@ module.exports = (_, argv) => ({
   plugins: [
     new ModuleFederationPlugin({
       name: "webpack_remote",
-      // library: {
-      //   type: "var", name: "webpack_remote"
-      // },
       filename: "remoteEntry.js",
       remotes: {},
       exposes: {
