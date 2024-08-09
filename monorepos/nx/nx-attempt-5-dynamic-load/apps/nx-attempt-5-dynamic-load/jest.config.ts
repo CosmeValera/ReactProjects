191657f47361535a776af64a06cc84762a7bad05
CosmeValera <cosmevalerareales@gmail.com>
2024-08-09 11:52:34 +0200

    Created nx 5 dynamic load

---
+++ monorepos/nx/nx-attempt-5-dynamic-load/apps/nx-attempt-5-dynamic-load/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-attempt-5-dynamic-load',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/nx-attempt-5-dynamic-load',
+};
