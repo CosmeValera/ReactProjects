e40e77befa4b86d0a4ddc2b555e48409b8ade8da
CosmeValera <cosmevalerareales@gmail.com>
2024-08-27 14:52:57 +0200

    src code in turborepo and shared library both have hmr

--- monorepos/turborepo/2-turboapp/apps/docs/app/page.tsx
+++ monorepos/turborepo/2-turboapp/apps/docs/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.tsx</code>
+            Empieza por editarr -&gt; <code>app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
--- monorepos/turborepo/2-turboapp/packages/ui/src/button.tsx
+++ monorepos/turborepo/2-turboapp/packages/ui/src/button.tsx
@@ -12,7 +12,7 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={() => alert(`Hello from faithfully ${appName} app!`)}
     >
       {children}
     </button>
