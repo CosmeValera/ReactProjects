fcecc124d9f43c497417fc621fc4c9dd1a6a05de
CosmeValera <cosmevalerareales@gmail.com>
2024-09-24 18:18:51 +0200

    npx tsx --watch, to replace nodemon with typescript. It would have saved me 3 hours xd

--- performance/tRPC/tRPC-1/server/src/index.ts
+++ performance/tRPC/tRPC-1/server/src/index.ts
@@ -1,15 +1,29 @@
 import express from 'express';
+import { initTRPC } from "@trpc/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('BACKEND: Express + TypeScript Server');
+const t = initTRPC.create();
+
+const appRouter = t.router({
+  sayHi: t.procedure.query(() => {
+    return "Hi";
+  }),
+  logToServer: t.procedure.input(v => {
+    if (typeof v === "string") {
+      return v;
+    }
+
+    throw new Error("Invalid input: Expected string")
+  }).mutation(req => {
+    console.log("Client says: ", req.input);
+    return true;
+  })
 });
 
-app.get('/user', (req, res) => {
-  res.send('BACKEND: User');
-});
+app.use("/trpc", createExpressMiddleware({ router: appRouter }))
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
