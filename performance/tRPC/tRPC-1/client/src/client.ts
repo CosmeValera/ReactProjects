febb3c88713a6c7deb62e89b4f2d35cc0baa864c
CosmeValera <cosmevalerareales@gmail.com>
2024-09-25 17:35:44 +0200

    You can add custom headers with authorization token for instance

--- performance/tRPC/tRPC-1/client/src/client.ts
+++ performance/tRPC/tRPC-1/client/src/client.ts
@@ -3,7 +3,10 @@ import { AppRouter } from "../../server/src/api"
 
 const client = createTRPCProxyClient<AppRouter>({
   links: [httpBatchLink({
-    url: "http://localhost:3000/trpc"
+    url: "http://localhost:3000/trpc",
+    headers: {
+      Authorization: "TOKEN"
+    }
   })]
 })
 
